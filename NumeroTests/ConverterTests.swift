af42824c02ba5e16f53a9308011384457f7430a9
esailers <eric@ericsailers.com>
2019-07-31 14:45:12 -0700

    Try another failed test

--- NumeroTests/ConverterTests.swift
+++ NumeroTests/ConverterTests.swift
@@ -44,7 +44,7 @@ class ConverterTests: XCTestCase {
     
     func testConversionForOne() {
         let result = converter.convert(2)
-        XCTAssertEqual(result, "I", "Conversion for 1 is incorrect")
+        XCTAssertEqual(result, "III", "Conversion for 1 is incorrect")
     }
     
     func testConversionForTwo() {
