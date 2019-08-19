daa6bbb8a50956dbbe3935342d07b51613c919e3
esailers <eric@ericsailers.com>
2019-08-19 15:30:38 -0700

    Remove extra space

--- NumeroTests/ConverterTests.swift
+++ NumeroTests/ConverterTests.swift
@@ -49,7 +49,7 @@ class ConverterTests: XCTestCase {
     
     func testConversionForTwo() {
         let result = converter.convert(2)
-        XCTAssertEqual(result, "II", "Conversion for 2 is incorrect") 
+        XCTAssertEqual(result, "II", "Conversion for 2 is incorrect")
     }
     
 }
